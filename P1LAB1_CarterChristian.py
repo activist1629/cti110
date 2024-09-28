0180d0f86e89973346bba5b7cc2c79a9273eb56e
activist1629 <carterc4971@student.faytechcc.edu>
2024-09-27 22:43:49 -0400

    Add files via upload

---
+++ P1LAB1_CarterChristian.py
@@ -0,0 +1,12 @@
+# Christian Cater
+# September 27, 2024
+# P1LAB1
+# This program asks the user for their first and last name and displays a welcome message.
+
+first_name = input('Please enter your first name: ')
+
+last_name = input('Please enter your first name: ')
+
+print(f'Hello, {first_name} {last_name}! Welcome to CTI-110')
+
+             
